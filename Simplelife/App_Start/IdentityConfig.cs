64cf697fb326269045c815aac757e2a434bbeda6
Nikita <nikita.nklv@gmail.com>
2016-11-22 10:49:01 +0300

    changed password reqirenments

--- Simplelife/App_Start/IdentityConfig.cs
+++ Simplelife/App_Start/IdentityConfig.cs
@@ -47,17 +47,15 @@ namespace Simplelife
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
                 AllowOnlyAlphanumericUserNames = false,
-                RequireUniqueEmail = true
+                RequireUniqueEmail = true                
             };
 
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
                 RequiredLength = 6,
-                RequireNonLetterOrDigit = true,
                 RequireDigit = true,
                 RequireLowercase = true,
-                RequireUppercase = true,
             };
 
             // Configure user lockout defaults
