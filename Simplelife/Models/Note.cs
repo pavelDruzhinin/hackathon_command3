c77bf638c23fcd9fefad175282fdfa0fb92d5972
Nikita <nikita.nklv@gmail.com>
2016-11-26 22:28:13 +0300

    Описал модели Категория и Заметка

---
+++ Simplelife/Models/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Simplelife.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public string Content { get; set; }
+    }
+}
