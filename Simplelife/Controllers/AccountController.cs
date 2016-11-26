50a12e350fe4aed69bc452a4d9434dde59fb068b
Nikita <nikita.nklv@gmail.com>
2016-11-26 22:24:19 +0300

    Переделал Layout

--- Simplelife/Controllers/AccountController.cs
+++ Simplelife/Controllers/AccountController.cs
@@ -83,7 +83,7 @@ namespace Simplelife.Controllers
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
-                    return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
+                    return RedirectToAction("SendCode", new { RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
                     ModelState.AddModelError("", "Invalid login attempt.");
