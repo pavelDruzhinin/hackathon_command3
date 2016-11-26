4919d1ba3413f95b8898ebde19e0e239800607d9
Nikita <nikita.nklv@gmail.com>
2016-11-26 16:28:30 +0300

    Добавил проект.

---
+++ Simplelife/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Simplelife.Startup))]
+namespace Simplelife
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
